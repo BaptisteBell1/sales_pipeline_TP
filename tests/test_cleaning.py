4c0eaf876d40a08616fceb4d61bdeca4faaa3954
bbellamy@aubay.com <bbellamy@aubay.com>
2025-12-22 14:10:50 +0000

    fix/update : update tests and fix CI/CD GitHub Action .yaml

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -4,18 +4,12 @@ from pyspark.sql import SparkSession
 
 @pytest.fixture(scope="session")
 def spark():
-    return (
-        SparkSession.builder
-        .master("local[2]")
-        .appName("pytest-spark")
-        .config("spark.ui.enabled", "false")
-        .getOrCreate()
-    )
+    return SparkSession.builder.getOrCreate()
 
 
 def test_clean_data_removes_nulls(spark):
     # Vérifie que la fonction de nettoyage supprime bien les lignes vides
-    df = spark.table("workspace.silver.silver_table")
+    df = spark.table("silver.silver_table")
 
     for col in df.columns:
         null_count = df.filter(df[col].isNull()).count()
